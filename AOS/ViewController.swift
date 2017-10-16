b98e0765bde3a73635813c64c1e6063b76f838af
Servet Simsek <i13090@student.htlwrn.ac.at>
2017-10-16 12:24:49 +0200

    Initial Commit

---
+++ AOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AOS
+//
+//  Created by SSIT on 16.10.17.
+//  Copyright © 2017 SSIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
