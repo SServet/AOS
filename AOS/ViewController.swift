f75dd018fd506b7c5b2c3517b14238d6374ede10
Servet Simsek <i13090@student.htlwrn.ac.at>
2017-11-20 12:00:43 +0100

    unnötige code Zeilen gelöscht

--- AOS/ViewController.swift
+++ AOS/ViewController.swift
@@ -45,38 +45,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
             self.performSegue(withIdentifier: "segueLoginClick", sender: self)
         }
     }
-    /*
-    @IBAction func segueLoginButton(_ sender: Any) {
-        if(Login_PW_Input.text != "" && Login_Mail_Input.text != ""){
-            self.performSegue(withIdentifier: "segueLoginClick", sender: self)
-        }
-        self.performSegue(withIdentifier: "segueLoginClick", sender: self)
-    }
-    */
-    @IBAction func segueEinstellungenButton(sender: UIButton) {
-        self.performSegue(withIdentifier: "segueTEinstellungenButton", sender: self)
-    }
-    
-    @IBAction func segueTicketsButton(sender: UIButton) {
-        self.performSegue(withIdentifier: "segueTicketsButton", sender: self)
-    }
-    @IBAction func segueProjekteButton(sender: UIButton) {
-        self.performSegue(withIdentifier: "segueProjekteButton", sender: self)
-        
-    }
-    @IBAction func segueArbeitsscheineButton(sender: UIButton) {
-        self.performSegue(withIdentifier: "segueArbeitsscheineButton", sender: self)
-    }
-    
-    /*
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // set title for Einstellungen screen
-        
-        if segue.identifier == "segueEinstellungen" {
-            
-            
-        }
-    }*/
     
 }
 
