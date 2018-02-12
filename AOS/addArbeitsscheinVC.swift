79739849281ada7a8721d034b77dc68d3532c774
Servet Simsek <i13090@student.htlwrn.ac.at>
2018-02-12 11:34:47 +0100

    Daten Aus Datenbank lesen

--- AOS/addArbeitsscheinVC.swift
+++ AOS/addArbeitsscheinVC.swift
@@ -42,6 +42,8 @@ class addArbeitsscheinVC: FormViewController {
     let URL_GET_ARTIKEL = "http://aos.ssit.at/php/v1/artikel.php"
     let URL_GET_TTYP = "http://aos.ssit.at/php/v1/ttyp.php"
     let URL_GET_TART = "http://aos.ssit.at/php/v1/tart.php"
+    let URL_POST_AS = "http://aos.ssit.at/php/v1/asInsert.php"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -115,6 +117,12 @@ class addArbeitsscheinVC: FormViewController {
                 $0.title = "Artikelanzahl"
                 $0.value = 1
             }
+            
+            <<< MultipleSelectorRow<String>(){
+                $0.title = "Vordefinierte Beschreibung"
+                $0.options = ["Protokolle OK", "Sicherung OK", "Update OK"]
+            }
+            
             <<< TextRow() {
                 $0.title = "Beschreibung"
             }
@@ -214,5 +222,109 @@ class addArbeitsscheinVC: FormViewController {
             <<< IntRow() {
                 $0.title = "Artikelanzahl"
         }
+            <<< ButtonRow("Senden"){
+                $0.title = "Senden"
+                $0.onCellSelection(self.buttontapped)
         }
+        }
+    func buttontapped(cell: ButtonCellOf<String>, row: ButtonRow){
+        let kid = getID(_tag: "Kunde")
+        let aid = getID(_tag: "Artikel")
+        let ttid = getID(_tag: "Termintyp")
+        let tid  = getID(_tag: "Taetigkeit")
+        let descr = getString(_tag: "Beschreibung")
+        let dfrom = getDate(_tag: String(describing: form.rowBy(tag: "Datum von")?.baseValue))
+        let kgrund = getString(_tag: "Kulanzgrund")
+        let kzeit = getString(_tag: "Kulanzzeit")
+        let defaultval = UserDefaults.standard
+        
+        print(dfrom)
+        
+        if let mid = defaultval.string(forKey: "userid"){
+            print(mid)
+        }
+        
+        if(checkID(id1: kid, id2: ttid, id3: tid) && descr != ""){
+            popup(title_: "Success", message_: "Arbeittschein angelegt!")
+        }else{
+            popup(title_: "Warnung", message_: "Es wurde keine Beschreibung geschrieben!")
+        }
+        
+    }
+    
+    func getID(_tag: String) -> Int{
+        let id: BaseRow? = form.rowBy(tag: _tag)
+        let val = id?.baseValue
+        if(val  != nil){
+            let a = String(describing: val)
+            let b = a.components(separatedBy: "(")
+            let c = b[1].components(separatedBy: ".")
+            return Int(c[0]) as! Int
+        }
+        return -1;
+    }
+    
+    func getDate(_tag: String)->Date{
+        let a = _tag.components(separatedBy: "(")
+        print(a)
+        if a.contains("nil") == false{
+            let b = a[1].components(separatedBy: " ")
+            let df = DateFormatter()
+            let date = df.date(from: b[0])
+            print(date)
+            if(date != nil){
+                return date!
+            }
+        }
+        return Date(timeIntervalSinceNow: 0)
+    }
+    
+    func getString(_tag: String) -> String{
+        let row: TextRow? = form.rowBy(tag: _tag)
+        if(row?.value != nil){
+            return (row?.value) as! String
+        }
+        return ""
+    }
+    
+    func checkID(id1: Int, id2: Int, id3: Int) -> Bool{
+        if(id1 == -1){
+            popup(title_: "Warnung", message_: "Es wurde kein Kunde ausgewählt!")
+            return false
+        }else if(id2 == -1){
+            popup(title_: "Warnung", message_: "Es wurde kein Termintyp ausgewählt!")
+            return false
+        }else if(id3 == -1){
+            popup(title_: "Warnung", message_: "Es wurde keine Tätigkeitsart ausgewählt!")
+            return false
+        }
+        return true
+    }
+    
+    func popup(title_: String, message_: String){
+        let alert = UIAlertController(title: title_ , message: message_, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func addAS(mid: Int, kid: Int, aid: Int, ttid: Int, tid: Int, descr: String, dfrom: Date, kgrund: String, kzeit: Int){
+        let AS: Parameters=[
+            "mid": mid,
+            "kid": kid,
+            "ttid": ttid,
+            "tid": tid,
+            "descr": descr,
+            "dfrom": dfrom,
+            "kgrund": kgrund,
+            "kzeit": kzeit
+        ]
+        
+        Alamofire.request(URL_POST_AS, method: .post, parameters: AS).responseJSON{
+            response in
+            print(response)
+        }
+        
+    }
 }
+
+
